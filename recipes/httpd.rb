aa68c97845e551780b1ef3cd4accc17c7f7f492b
lyfenwebos <mrexoduso@gmail.com>
2017-09-05 00:27:16 +0300

    Httpd Recipe updated - PHP added

--- recipes/httpd.rb
+++ recipes/httpd.rb
@@ -14,3 +14,13 @@ end
 #  source 'default.conf.erb'
 #end
 
+# Install the mod_php5 Apache module.
+httpd_module 'php5' do
+  instance 'default'
+end
+
+# Install php5-mysql.
+package 'php5-mysql' do
+  action :install
+  notifies :restart, 'httpd_service[default]'
+end
